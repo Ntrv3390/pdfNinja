820531399071dd5458618c020842344601f718e3
Ntrv3390 <78497133+Ntrv3390@users.noreply.github.com>
2024-02-28 08:09:59 +0530

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const year = new Date().getFullYear();
 const Footer = () => {
     return (
         <div id="footer" className='mt-[4rem] mb-[0.7rem] font-semibold h-[3rem] w-auto'>
-            <p className='px-3 md:px-0 text-center text-gray-700'>Copyright &copy; {year} pdfNinja. A product by <Link href="https://avitize-digital.vercel.app" target="_blank"><strong>Avitize Digital</strong></Link>.</p>
+            <p className='px-3 md:px-0 text-center text-gray-700'>Copyright &copy; {year} pdfNinja.</p>
         </div>
     );
 }
