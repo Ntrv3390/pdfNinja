91acfbf766b33da50893f4e6dad298e69dd09041
Ntrv3390 <78497133+Ntrv3390@users.noreply.github.com>
2024-07-12 19:21:30 +0530

    fix

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -27,7 +27,7 @@ const UserAccountNav = async ({
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild className="overflow-visible">
-        <Button className="rounded-full h-10 w-10 aspect-square bg-orange-400">
+        <Button className="rounded-md px-6 py-4 aspect-square bg-orange-400">
           Profile
         </Button>
       </DropdownMenuTrigger>
