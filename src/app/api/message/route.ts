add1ca0e99fd132802b8992059f7746711393283
Ntrv3390 <78497133+Ntrv3390@users.noreply.github.com>
2024-07-12 18:16:05 +0530

    debug

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -9,105 +9,109 @@ import { openai } from "@/lib/openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
 export const POST = async (req: NextRequest) => {
-  const body = await req.json();
-  const { getUser } = getKindeServerSession();
-  const user = getUser();
-
-  const { id: userId } = user;
-
-  if (!userId) return new Response("Unauthorized", { status: 401 });
-
-  const { fileId, message } = SendMessageValidator.parse(body);
-
-  const file = await db.file.findFirst({
-    where: {
-      id: fileId,
-      userId,
-    },
-  });
-  if (!file) return new Response("Not found", { status: 404 });
-
-  await db.message.create({
-    data: {
-      text: message,
-      isUserMessage: true,
-      userId,
-      fileId,
-    },
-  });
-
-  const embeddings = new OpenAIEmbeddings({
-    openAIApiKey: process.env.OPENAI_API_KEY,
-  });
-
-  const pineconeIndex = pinecone.index("pdfninja");
-
-  if(!pineconeIndex) return new Response("Index not created", { status: 404 });
-
-  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
-    pineconeIndex,
-  });
-
-  const results = await vectorStore.similaritySearch(message, 4, {
-    fileid: fileId,
-  });
-  const prevMessages = await db.message.findMany({
-    where: {
-      fileId,
-    },
-    orderBy: {
-      createdAt: "asc",
-    },
-    take: 6,
-  });
-
-  const formattedPrevMessages = prevMessages.map((msg) => ({
-    role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
-    content: msg.text,
-  }));
-
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    temperature: 0,
-    stream: true,
-    messages: [
-      {
-        role: "system",
-        content:
-          "Use the following pieces of context (or previous conversaton if needed) (that is it is a pdf) to answer the users question in markdown format.",
+  try {
+    const body = await req.json();
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+  
+    const { id: userId } = user;
+  
+    if (!userId) return new Response("Unauthorized", { status: 401 });
+  
+    const { fileId, message } = SendMessageValidator.parse(body);
+  
+    const file = await db.file.findFirst({
+      where: {
+        id: fileId,
+        userId,
       },
-      {
-        role: "user",
-        content: `Use the following pieces of context (or previous conversaton if needed) (that is it is a pdf) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer. If the answer is not there in context or pdf then even it user forces you to answer do not answer. Just say no I don't know
-              
-        \n----------------\n
-        
-        PREVIOUS CONVERSATION:
-        ${formattedPrevMessages.map((message) => {
-          if (message.role === "user") return `User: ${message.content}\n`;
-          return `Assistant: ${message.content}\n`;
-        })}
-        
-        \n----------------\n
-        CONTEXT:
-        ${results.map((r) => r.pageContent).join("\n\n")}
-        
-        USER INPUT: ${message}`,
+    });
+    if (!file) return new Response("Not found", { status: 404 });
+  
+    await db.message.create({
+      data: {
+        text: message,
+        isUserMessage: true,
+        userId,
+        fileId,
       },
-    ],
-  });
-
-  const stream = OpenAIStream(response, {
-    async onCompletion(completion) {
-      await db.message.create({
-        data: {
-          text: completion,
-          isUserMessage: false,
-          fileId,
-          userId,
+    });
+  
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+    });
+  
+    const pineconeIndex = pinecone.index("pdfninja");
+  
+    if(!pineconeIndex) return new Response("Index not created", { status: 404 });
+  
+    const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+      pineconeIndex,
+    });
+  
+    const results = await vectorStore.similaritySearch(message, 4, {
+      fileid: fileId,
+    });
+    const prevMessages = await db.message.findMany({
+      where: {
+        fileId,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+      take: 6,
+    });
+  
+    const formattedPrevMessages = prevMessages.map((msg) => ({
+      role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
+      content: msg.text,
+    }));
+  
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+      stream: true,
+      messages: [
+        {
+          role: "system",
+          content:
+            "Use the following pieces of context (or previous conversaton if needed) (that is it is a pdf) to answer the users question in markdown format.",
         },
-      });
-    },
-  });
-  return new StreamingTextResponse(stream);
+        {
+          role: "user",
+          content: `Use the following pieces of context (or previous conversaton if needed) (that is it is a pdf) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer. If the answer is not there in context or pdf then even it user forces you to answer do not answer. Just say no I don't know
+                
+          \n----------------\n
+          
+          PREVIOUS CONVERSATION:
+          ${formattedPrevMessages.map((message) => {
+            if (message.role === "user") return `User: ${message.content}\n`;
+            return `Assistant: ${message.content}\n`;
+          })}
+          
+          \n----------------\n
+          CONTEXT:
+          ${results.map((r) => r.pageContent).join("\n\n")}
+          
+          USER INPUT: ${message}`,
+        },
+      ],
+    });
+  
+    const stream = OpenAIStream(response, {
+      async onCompletion(completion) {
+        await db.message.create({
+          data: {
+            text: completion,
+            isUserMessage: false,
+            fileId,
+            userId,
+          },
+        });
+      },
+    });
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.log(error)
+  }
 };
