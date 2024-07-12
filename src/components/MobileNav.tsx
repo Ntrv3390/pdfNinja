46f7770016e4ef48121f1b89dc32ac5dd601d300
Ntrv3390 <78497133+Ntrv3390@users.noreply.github.com>
2024-07-12 18:46:31 +0530

    fix

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,71 +1,65 @@
-'use client'
+"use client";
 
-import { ArrowRight, Menu } from 'lucide-react'
-import Link from 'next/link'
-import {
-  LoginLink,
-  RegisterLink,
-} from '@kinde-oss/kinde-auth-nextjs/server'
-import { usePathname } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { ArrowRight, Menu } from "lucide-react";
+import Link from "next/link";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const MobileNav = ({ isAuth }: { isAuth: boolean }) => {
-  const [isOpen, setOpen] = useState<boolean>(false)
+  const [isOpen, setOpen] = useState<boolean>(false);
 
-  const toggleOpen = () => setOpen((prev) => !prev)
+  const toggleOpen = () => setOpen((prev) => !prev);
 
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   useEffect(() => {
-    if (isOpen) toggleOpen()
-  }, [pathname])
+    if (isOpen) toggleOpen();
+  }, [pathname]);
 
   const closeOnCurrent = (href: string) => {
     if (pathname === href) {
-      toggleOpen()
+      toggleOpen();
     }
-  }
+  };
 
   return (
-    <div className='sm:hidden'>
+    <div className="sm:hidden">
       <Menu
         onClick={toggleOpen}
-        className='relative z-50 h-5 w-5 text-zinc-700'
+        className="relative z-50 h-5 w-5 text-zinc-700"
       />
 
       {isOpen ? (
-        <div className='fixed animate-in slide-in-from-top-5 fade-in-20 inset-0 z-0 w-full'>
-          <ul className='absolute bg-white border-b border-zinc-200 shadow-xl grid w-full gap-3 px-10 pt-20 pb-8'>
+        <div className="fixed animate-in slide-in-from-top-5 fade-in-20 inset-0 z-0 w-full">
+          <ul className="absolute bg-white border-b border-zinc-200 shadow-xl grid w-full gap-3 px-10 pt-20 pb-8">
             {!isAuth ? (
               <>
                 <li>
                   <RegisterLink
-                    onClick={() =>
-                      closeOnCurrent('/sign-up')
-                    }
-                    className='flex items-center w-full font-semibold text-orange-600'>
+                    onClick={() => closeOnCurrent("/sign-up")}
+                    className="flex items-center w-full font-semibold text-orange-600"
+                  >
                     Get started
-                    <ArrowRight className='ml-2 h-5 w-5' />
+                    <ArrowRight className="ml-2 h-5 w-5" />
                   </RegisterLink>
                 </li>
-                <li className='my-3 h-px w-full bg-gray-300' />
+                <li className="my-3 h-px w-full bg-gray-300" />
                 <li>
                   <LoginLink
-                    onClick={() =>
-                      closeOnCurrent('/sign-in')
-                    }
-                    className='flex items-center w-full font-semibold'>
+                    onClick={() => closeOnCurrent("/sign-in")}
+                    className="flex items-center w-full font-semibold"
+                  >
                     Sign in
                   </LoginLink>
                 </li>
-                <li className='my-3 h-px w-full bg-gray-300' />
+                <li className="my-3 h-px w-full bg-gray-300" />
                 <li>
                   <Link
-                    onClick={() =>
-                      closeOnCurrent('/pricing')
-                    }
-                    className='flex items-center w-full font-semibold'
-                    href='/pricing'>
+                    onClick={() => closeOnCurrent("/pricing")}
+                    className="flex items-center w-full font-semibold"
+                    href="/pricing"
+                  >
                     Pricing
                   </Link>
                 </li>
@@ -74,19 +68,19 @@ const MobileNav = ({ isAuth }: { isAuth: boolean }) => {
               <>
                 <li>
                   <Link
-                    onClick={() =>
-                      closeOnCurrent('/dashboard')
-                    }
-                    className='flex items-center w-full font-semibold'
-                    href='/dashboard'>
+                    onClick={() => closeOnCurrent("/dashboard")}
+                    className="flex items-center w-full font-semibold"
+                    href="/dashboard"
+                  >
                     Dashboard
                   </Link>
                 </li>
-                <li className='my-3 h-px w-full bg-gray-300' />
+                <li className="my-3 h-px w-full bg-gray-300" />
                 <li>
                   <Link
-                    className='flex items-center w-full font-semibold'
-                    href='/sign-out'>
+                    className="flex items-center w-full font-semibold"
+                    href="/sign-out"
+                  >
                     Sign out
                   </Link>
                 </li>
@@ -96,7 +90,7 @@ const MobileNav = ({ isAuth }: { isAuth: boolean }) => {
         </div>
       ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default MobileNav
+export default MobileNav;
