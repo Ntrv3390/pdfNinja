4fb873c2adf06e449379cb69bb1685a43ef53d54
Mohammed Puthawala <78497133+Ntrv3390@users.noreply.github.com>
2024-08-02 18:35:52 +0530

    Update Navbar UI Changes

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -50,7 +50,7 @@ const UserAccountNav = async ({
           <Link href="/dashboard">Dashboard</Link>
         </DropdownMenuItem>
 
-        <DropdownMenuItem asChild>
+{/*         <DropdownMenuItem asChild>
           {subscriptionPlan?.isSubscribed ? (
             <Link href="/dashboard/billing">Manage Subscription</Link>
           ) : (
@@ -58,7 +58,7 @@ const UserAccountNav = async ({
               Upgrade <Gem className="text-blue-600 h-4 w-4 ml-1.5" />
             </Link>
           )}
-        </DropdownMenuItem>
+        </DropdownMenuItem> */}
 
         <DropdownMenuSeparator />
 
